b09d62e71791c1f539c40f3674e4c6bd017cd770
OmriYahav <122606148+OmriYahav@users.noreply.github.com>
2023-03-06 10:34:47 +0200

    Fix Hints.

--- FortiCONF.py
+++ FortiCONF.py
@@ -5,8 +5,8 @@ import FortiGET
 # Replace with your FortiGate's IP address:port and API key
 # Fortigates = dict(Client1='IP:PORT', Client2='IP:PORT')
 # Apikeys = dict(Client1='API_KEY', Client2='API_KEY')
-Fortigates = dict(Client1='',Client2='')
-Apikeys = dict(Client1='',Client2='')
+Fortigates = dict(Client1='IP:PORT', Client2='IP:PORT')
+Apikeys = dict(Client1='API_KEY', Client2='API_KEY')
 now = datetime.now()
 dt_string = now.strftime("Date : %d/%m/%Y \nTime: %H:%M:%S")
 
